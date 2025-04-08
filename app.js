ed01d4737692eb05ab542be5ccbcc07c9f6d2fe0
Kumuthiny <kumuthiny0726@gmail.com>
2025-04-08 16:43:38 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+const userRoutes = require('./routes/userRoutes');
+const postRoutes = require('./routes/postRoutes');
+
+// Use routes
+app.use('/user', userRoutes);
+app.use('/post', postRoutes);
+
+// Handle invalid routes (bonus point for Q3)
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
